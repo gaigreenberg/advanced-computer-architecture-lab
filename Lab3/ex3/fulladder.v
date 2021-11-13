281e79a1756f29ad895957c5c1c46c3b6c425e41
User <jungob94@gmail.com>
2021-11-13 17:45:05 +0200

    finished ex3

--- Lab3/ex3/fulladder.v
+++ Lab3/ex3/fulladder.v
@@ -4,5 +4,15 @@ module fulladder( sum, co, a, b, ci);
   output  sum, co;
 
   // FILL HERE
+  wire s1,c2,c3,c4;
+  xor (s1,a,b); //s1 = a + b
+  xor (sum, s1, ci); // sum = s1 + ci
+
+  // find the carry by testing all the possible combinations of 2 input params - if one of the options is '1' - carry is '1'
+
+  and(c1,a,ci);
+  and(c2, b, ci);
+  and(c3, a, b);
+  or (co, c1, c2, c3);
 
 endmodule
